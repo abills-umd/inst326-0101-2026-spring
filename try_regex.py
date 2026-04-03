42058e2f64fb1b6ddb4700da9c783e0cde803d7a
Aric Bills <abills@umd.edu>
2026-04-03 09:34:30 -0400

    moved code to function; added if __name__ == "__main__" statement

--- try_regex.py
+++ try_regex.py
@@ -1,6 +1,11 @@
 import re
 
-expr = r"\w+"
-response = input("Give me a string: ")
-match = re.search(expr, response)
-print("The match is " + match[0] if match else "No match!")
+def try_regex():
+    expr = r"\w+"
+    response = input("Give me a string: ")
+    match = re.search(expr, response)
+    print("The match is " + match[0] if match else "No match!")
+
+
+if __name__ == "__main__":
+    try_regex()
